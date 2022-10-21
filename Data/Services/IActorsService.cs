eb8b2b5bd641f60cc36c81522f4cdb80a6a30cb5
ADMINPC\PC <faruk.selimovic85@gmail.com>
2022-10-21 15:26:09 +0200

    Add project files.

---
+++ Data/Services/IActorsService.cs
@@ -0,0 +1,18 @@
+﻿using eTikets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTikets.Data.Services
+{
+    public interface IActorsService
+    {
+        Task<IEnumerable<Actor>> GetAllAsync();
+        Task<Actor> GetByIdAsync(int id);
+        Task AddAsync(Actor actor);
+        Task<Actor> UpDateAsync(int id, Actor newActor);
+        Task DeleteAsync(int id);
+
+    }
+}
